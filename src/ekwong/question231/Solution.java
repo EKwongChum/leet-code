5dac6b84d7bf8f18b2cc9c9225da81b8bcd9a3d7
E Kwong MBP <ekwongchum@gmail.com>
2019-08-28 09:59:19 +0800

    看起来不用遍历32次，遍历12次就够了

--- src/ekwong/question231/Solution.java
+++ src/ekwong/question231/Solution.java
@@ -1,13 +1,16 @@
 package ekwong.question231;
 
+import java.nio.ByteBuffer;
+
 public class Solution {
 
     /**
      * 耗时 1 ms，哈哈，又一次战胜100%
+     *
      * @param n
      * @return
      */
-    public static boolean isPowerOfTwo(int n) {
+    public static boolean isPowerOfTwo2(int n) {
 
         if (n < 0) {
             return false;
@@ -22,6 +25,51 @@ public class Solution {
         }
 
         return false;
+    }
+
+    public static boolean isPowerOfTwo3(int n) {
+
+        if (n < 0) {
+            return false;
+        }
+        boolean notZero = false;
+        byte notZeroByte = 0;
+        byte[] bytes = ByteBuffer.allocate(4).putInt(n).array();
+        for (byte b : bytes) {
+            if (b != 0) {
+                if (notZero) {
+                    return false;
+                }
+                notZero = true;
+                notZeroByte = b;
+            }
+        }
+        byte pow = 1;
+        for (int i = 0; i < 8; i++) {
+            if ((notZeroByte ^ pow) == 0) {
+                return true;
+            } else {
+                pow <<= 1;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isPowerOfTwo(int n) {
+
+        if (n <= 0) {
+            return false;
+        }
+        boolean hasUp = false;
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                if (hasUp) {
+                    return false;
+                }
+                hasUp = true;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
