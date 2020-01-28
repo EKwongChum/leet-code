bc838f4b95a44618d5d7d692fc36f09842954568
E Kwong MBP <ekwongchum@gmail.com>
2020-01-28 22:40:05 +0800

    no message

--- src/ekwong/question119/Solution.java
+++ src/ekwong/question119/Solution.java
@@ -1,9 +1,9 @@
 package ekwong.question119;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
@@ -18,11 +18,38 @@ import java.util.Queue;
  * Could you optimize your algorithm to use only O(k) extra space?
  */
 public class Solution {
+
+
     public List<Integer> getRow(int rowIndex) {
+        List<Integer> integerList = new LinkedList<>();
+        if (rowIndex == 0) {
+            integerList.add(1);
+            return integerList;
+        }
+        for (int i = 0; i <= rowIndex; i++) {
+            BigInteger n = factorial(rowIndex);
+            BigInteger m = factorial(i).multiply(factorial(rowIndex - i));
+            integerList.add((n.divide(m)).intValue());
+        }
+        return integerList;
+    }
+
+    private BigInteger factorial(int n) {
+        if (n == 0) {
+            return new BigInteger("1");
+        } else if (n == 1) {
+            return new BigInteger("1");
+        } else {
+            BigInteger bigInteger = new BigInteger("" + n);
+            return bigInteger.multiply(factorial(n - 1));
+        }
+    }
+
+    public List<Integer> getRow1(int rowIndex) {
         List<Integer> tmpList = new ArrayList<>();
         List<Integer> curList = new ArrayList<>();
         tmpList.add(1);
-        for (int i = 0; i <= rowIndex+1; i++) {
+        for (int i = 0; i <= rowIndex + 1; i++) {
             for (int j = 0; j < i; j++) {
                 if (j == 0) {
                     curList.add(1);
@@ -37,5 +64,11 @@ public class Solution {
             curList.clear();
         }
         return tmpList;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.getRow(13));
     }
 }
