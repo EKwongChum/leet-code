3c3c9b572b6a5116f161449c60d4186ecf3f35b2
ekwong <ekwongchum@qq.com>
2020-03-03 23:20:11 +0800

    爬楼梯

---
+++ src/ekwong/question70/Solution.java
@@ -0,0 +1,21 @@
+package ekwong.question70;
+
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int first = 1;
+        int second = 2;
+        int third;
+        for (int i = 3; i <= n; i++) {
+            third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}
