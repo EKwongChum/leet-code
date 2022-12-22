7786d21d1bbea631a2e5ee3ca98418e8b9ba87d3
ejqin <ejqin@coremail.cn>
2022-12-22 23:19:12 +0800

    94题

---
+++ src/ekwong/question94/Solution.java
@@ -0,0 +1,21 @@
+package ekwong.question94;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        if (root.left != null) {
+            result.addAll(inorderTraversal(root.left));
+        }
+        result.add(root.val);
+        if (root.right != null) {
+            result.addAll(inorderTraversal(root.right));
+        }
+        return result;
+    }
+}
